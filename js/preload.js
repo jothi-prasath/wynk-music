afd25bcc325a0a3fa357696c5904cfe07040df0d
jothi prasath <jothiprasath2@gmail.com>
2021-01-20 15:14:41 +0530

    added custom title bar

--- js/preload.js
+++ js/preload.js
@@ -4,7 +4,6 @@ const url = require('url');
 const customTitlebar = require('custom-electron-titlebar');
 
 
-
 window.addEventListener('DOMContentLoaded', () => {
   const titlebar = new customTitlebar.Titlebar({
     backgroundColor: customTitlebar.Color.fromHex('#2f3241'),
