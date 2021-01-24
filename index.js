77d924156cf857ad5fdec09bd67d826c11f1c767
jothi-prasath <jothiprasath2@gmail.com>
2021-01-24 20:07:03 +0530

    add tray icon

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
-const { app, BrowserWindow, nativeImage, globalShortcut } = require('electron');
+const { app, Menu, BrowserWindow, nativeImage, Tray } = require('electron');
 const path = require('path');
 const myUserAgent = 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.141 Safari/537.36'
 
 
 
-let mainWindow
+let mainWindow, tray
 
 let iconImage = nativeImage.createFromPath(
   path.join(__dirname, "/build/icon.png")
@@ -30,10 +30,45 @@ function createWindow() {
   mainWindow.webContents.userAgent = myUserAgent
   mainWindow.setSize(1250, 600, false)
   mainWindow.loadURL('http://wynk.in')
-  mainWindow.once('ready-to-show', () => {
-    mainWindow.show()
-  })
+  mainWindow.once('ready-to-show', () => mainWindow.show())
 }
+
+app.whenReady().then(() => {
+  tray = new Tray(iconImage)
+  tray.setToolTip('Wynk')
+  tray.on('click', () => mainWindow.show())
+
+  // Create context menu for tray icon
+  let trayContextMenu = Menu.buildFromTemplate([
+    {
+      label: "Maximize",
+      click() {
+        if (mainWindow) {
+          // Show the main window
+          mainWindow.show();
+          // Focus the main window
+          mainWindow.focus();
+        }
+      }
+    },
+    {
+      label: "Minimize to Tray",
+      click() {
+        // Hide the main window i.e. minimize to tray
+        mainWindow.hide();
+      }
+    },
+    {
+      label: "Exit",
+      click() {
+        // Quit the app
+        app.exit(0);
+      }
+    }
+  ])
+
+  tray.setContextMenu(trayContextMenu)
+})
 
 app.on('ready', createWindow)
 
