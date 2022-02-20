c00f7484347af5c4ef8fbc4a694c5238b758587a
jothi prasath <jothiprasath2@gmail.com>
2022-02-20 19:07:45 +0530

    Added comments

--- src/js/preload.js
+++ src/js/preload.js
@@ -3,11 +3,11 @@ const url = require('url');
 
 const customTitlebar = require('custom-electron-titlebar');
 
-
+// Custom title bar
 window.addEventListener('DOMContentLoaded', () => {
   const titlebar = new customTitlebar.Titlebar({
     backgroundColor: customTitlebar.Color.fromHex('#202224'),
-    icon: true,
-    menu: false
+    icon: true, // Icon on top left corner in title bar
+    menu: false // Menu disabled because of minimal look
   })
 })
